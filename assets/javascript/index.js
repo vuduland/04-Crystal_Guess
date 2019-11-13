6e2c14b2b66b0009fbc9869a455a76c46032d2a3
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-11-13 02:40:35 -0600

    added script to test.html; not working

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,18 +4,20 @@ var game = {
     lossCount: 0,
     playAgain: "You've lost. Insert floppy disk to continue. Just kidding. Press Ok.",
     winPlayAgain: "You won! Press Ok to play again!",
-    randNum: $('#')
 },
 
-var min = 1;
-var max = 12;
-var maxTwo = 120;
+var minNum = 1;
+var maxNum = 12;
 var minTwo = 19;
-function crystalNum(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
+var maxTwo = 120;
+function randNum(min, max) {
+    min = Math.ceil(minTwo);
+    max = Math.floor(maxTwo);
     return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
-
+console.log(randNum(minTwo, maxTwo));
 
 $('')
+
+
+
