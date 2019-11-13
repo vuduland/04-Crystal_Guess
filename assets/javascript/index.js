ae59865db045600477406bdc75690aeea00c81d8
Victor Uland <53414263+vuduland@users.noreply.github.com>
2019-11-13 02:04:21 -0600

    added new index.js

---
+++ assets/javascript/index.js
@@ -0,0 +1,21 @@
+var game = {
+    guessedNums: [],
+    winCount: 0,
+    lossCount: 0,
+    playAgain: "You've lost. Insert floppy disk to continue. Just kidding. Press Ok.",
+    winPlayAgain: "You won! Press Ok to play again!",
+    randNum: 0,
+},
+
+var min = 1;
+var max = 12;
+var maxTwo = 120;
+var minTwo = 19;
+function crystalNum(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
+}
+
+
+$('')
