a346040aea40d9a730e936be1c2ab0f70046d9df
Victor <53414263+vuduland@users.noreply.github.com>
2019-12-03 17:48:28 -0600

    game works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,85 +1,107 @@
 var game = {
-  crystals: [0, 0, 0, 0],
-  crysParams: [1, 12],
+  // properties
+  crystals: [{ value: 0 }, { value: 0 }, { value: 0 }, { value: 0 }],
+  crystalParams: [1, 12],
   targetParams: [19, 120],
   wins: 0,
   losses: 0,
-  currentScore: 0,
-  crystal: [{ value: 0 }, { value: 0 }, { value: 0 }, { value: 0 }],
+  targetVal: 0,
+  playerVal: 0,
   lossPlayAgain: "You've lost. Insert floppy disk to continue. Just kidding. Press Ok.",
   winPlayAgain: 'You won! Press Ok to play again!',
 
   // methods
-  cumulativeNum: function (crystalsIndex) {
-    this.currentScore += crystalsIndex;
-    return this.currentScore;
+  clearValues: function () {
+    this.targetVal = 0;
+    this.playerVal = 0;
   },
-  generatedNum: function (min, max) {
+  // accumulated player values
+  playerNum: function (crystalsIndex) {
+    this.playerVal += crystalsIndex;
+    return this.playerVal;
+  },
+
+  // generate random number
+  randNum: function (min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
   },
-  updateGame: function () { //add params?
-    $('#generatedNum').append(this.generatedNum(this.targetParams[0], this.targetParams[1])),
-      $('#wins').append(this.wins),
-      $('#losses').append(this.losses),
-      $('#cumulativeNum').append(this.cumulativeNum);
+
+  // reset game to play again
+  updateGame: function () {
+    this.clearValues();
+    this.targetVal = this.randNum(this.targetParams[0], this.targetParams[1]),
+
+      $('#randNum').text(this.targetVal),
+      $('#wins').text(this.wins),
+      $('#losses').text(this.losses),
+      $('#playerNum').append(this.playerNum);
+    // apply value to crystals
     for (let i = 0; i <= 3; i++) {
-      this.crystal[i].value = game.generatedNum(this.crysParams[0], this.crysParams[1]);
-
+      this.crystals[i].value = game.randNum(this.crystalParams[0], this.crystalParams[1]);
     }
-    console.log(this.crystal);
-
+    return this.targetVal;
   },
 
-};
-game.updateGame();
+  // checks playerNum for equality; if equal player wins, else not
+  checkScore: function (crystalVal, targetVal) {
+    this.playerVal += crystalVal;
 
+    if (this.playerVal === targetVal) {
+      alert(this.winPlayAgain);
+      this.wins++;
+      this.updateGame();
+    } else if (this.playerVal > targetVal) {
+      alert(this.lossPlayAgain);
+      this.losses++;
+      this.updateGame();
+    }
+  }
+};
+
+// on-click functions
 $('#button1').click(function () {
-  game.currentScore += game.crystal[0].value;
-  console.log(game.currentScore + ' currentScore');
-  console.log(game.crystal[0].value + ' button1');
-  $('#cumulativeNum').html('Your number: ' + game.currentScore);
+  var crystalVal = game.crystals[0].value;
+  var targetVal = game.targetVal;
+  game.checkScore(crystalVal, targetVal);
+  $('#playerNum').text(game.playerVal);
 }),
+
   $('#button2').click(function () {
-    game.currentScore += game.crystal[1].value;
-    console.log(game.currentScore + ' currentScore');
-    console.log(game.crystal[1].value + ' button2');
-    $('#cumulativeNum').html('Your number: ' + game.currentScore);
+    var crystalVal = game.crystals[1].value;
+    var targetVal = game.targetVal;
+    game.checkScore(crystalVal, targetVal);
+    $('#playerNum').text(game.playerVal);
   }),
+
   $('#button3').click(function () {
-    game.currentScore += game.crystal[2].value;
-    console.log(game.currentScore + ' currentScore');
-    console.log(game.crystal[2].value + ' button3');
-    $('#cumulativeNum').html('Your number: ' + game.currentScore);
+    var crystalVal = game.crystals[2].value;
+    var targetVal = game.targetVal;
+    game.checkScore(crystalVal, targetVal);
+    $('#playerNum').text(game.playerVal);
   }),
+
   $('#button4').click(function () {
-    game.currentScore += game.crystal[3].value;
-    console.log(game.currentScore + ' currentScore');
-    console.log(game.crystal[3].value + ' button2');
-    $('#cumulativeNum').html('Your number: ' + game.currentScore);
+    var crystalVal = game.crystals[3].value;
+    var targetVal = game.targetVal;
+    game.checkScore(crystalVal, targetVal);
+    console.log(crystalVal + ' and ' + targetVal);
+    console.log(game.playerVal + ' playerVal');
+    console.log(game.crystals[3].value + ' button4');
+    $('#playerNum').text(game.playerVal);
   })
 
 
+game.updateGame();
 
-  // on-click start button
-  // $('#getStarted').click(function () {
-  //   for (let i = 0; i < 4; i++) {
-  //     var num = game.generatedNum(min, max);
-  //     if (game.crystals.includes(num)) {
-  //       game.crystals.push(num);
-  //       console.log(game.crystals + ' first log');
-  //     }
-  //   };
-  // },
-  // )
 
 /*
 //     3. Here's how the app works:
 
 //         * There will be four crystals displayed as buttons on the page. done
 
-//    * The player will be shown a random number at the start of the game. random function
+//    * The player will be shown a random number at the start of the game. random function == done
 
 //    * When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 
